b21d3fce512033800c8dc6a6282b144071cef705
Ken <ken205115331@gmail.com>
2024-06-08 17:05:33 +0800

    model test

--- pipeline.js
+++ pipeline.js
@@ -5,16 +5,23 @@ let submitProblem = require('./submit_problem');
 
 dotenv.config();
 const OJ_URL = "http://134.208.3.66/"
-const problemID = "05";
 const modelName = "gpt-4"; //"gemini-1.5-pro" or "gpt-4"
 const problemPath = "./data/problem.json";
 const answerPath = './data/answer.txt';
 const Username = modelName === "gpt-4" ? process.env.OJ_GPT_USERNAME : process.env.OJ_GEMINI_USERNAME;
 const Password = modelName === "gpt-4" ? process.env.OJ_GPT_PASSWORD : process.env.OJ_GEMINI_PASSWORD;
 
-(async () => {
+async function model_test(problemID) {
   await getProblem(problemID, OJ_URL);
   const answer = await solveProblem.solve(problemPath, modelName);
-  await solveProblem.explain(problemPath, answer, modelName);
+  //await solveProblem.explain(problemPath, answer, modelName);
   await submitProblem(answerPath, problemID, OJ_URL,{ username: Username, password: Password });
+}
+
+(async () => {
+  for(let i = 1; i <= 51; i++){
+    let problemID = i.toString().padStart(2, '0');
+    console.log("Problem ID: " + problemID);
+    await model_test(problemID);
+  }
 })();
